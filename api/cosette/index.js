8b3f4025723e6ef0055526a007c147f7e1ad7a48
Nicolas CLERC <nicolas.clerc@sablefin.net>
2022-03-18 17:20:47 +0100

    update json api

--- api/cosette/index.js
+++ api/cosette/index.js
@@ -2,6 +2,6 @@ module.exports = async function (context, req) {
     context.log('Ma misérable function qui fait la cosette vient de s\'executer');
 
     context.res.json({
-        text:"Lorsque Pierre arriva sur la place de Grève, il était transi. Aussi se hâta-t-il de s\'approcher du feu de joie qui brûlait magnifiquement au milieu de la place. Mais une foule considérable faisait cercle à l\'entour."
+        extrait:"Lorsque Pierre arriva sur la place de Grève, il était transi. Aussi se hâta-t-il de s\'approcher du feu de joie qui brûlait magnifiquement au milieu de la place. Mais une foule considérable faisait cercle à l\'entour."
     });
 }
